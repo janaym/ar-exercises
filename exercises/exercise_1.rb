7eb2bf369d38062016a1ba8cb3792ec96f6dc29a
Janay Ma <janay.ma7@gmail.com>
2023-10-23 16:47:28 -0700

    complete exercise 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,4 +3,31 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
-# Your code goes below here ...
+stores = [
+  { 
+    name: "Burnaby",
+    annual_revenue: "300000",
+    womens_apparel: true,
+    mens_apparel: false
+  },
+
+  {
+    name: "Richmond",
+    annual_revenue: "1260000",
+    womens_apparel: true,
+    mens_apparel: false
+  },
+
+  {
+    name: "Gastown",
+    annual_revenue: "190000",
+    womens_apparel: false,
+    mens_apparel: true
+  }
+]
+
+stores.each do |store|
+  new_store = Store.create(store)
+end
+
+puts "Number of stores: #{Store.count}"
