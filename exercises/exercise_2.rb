0a821ac7442499bd61457f2390fd0490bc075c7e
Janay Ma <janay.ma7@gmail.com>
2023-10-23 16:50:45 -0700

    complete question 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,11 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+store1 = Store.find_by(id: 1)
+
+puts "Question 2: found store #{store1.name}"
+
+store1.name = "Victoria"
+
+puts "changed store name to #{store1.name}"
