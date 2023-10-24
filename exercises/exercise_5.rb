c684e72ee41a60535b4452d82935e65eddb18904
Janay Ma <janay.ma7@gmail.com>
2023-10-23 17:34:57 -0700

    complete q5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,8 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+puts "Question 5: the total annual revenue is: ", Store.sum(:annual_revenue)
+puts "The average store annual revenue is: ", Store.sum(:annual_revenue)/Store.count
+puts "Stores making more than one mil: ", Store.where("annual_revenue > ?", 1000000).count()
+
